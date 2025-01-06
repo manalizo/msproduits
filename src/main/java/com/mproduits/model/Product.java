34b49a3017371d71fb76679a37760b02e8519a00
manalizo <yumimanal8@gmail.com>
2025-01-06 21:01:08 +0100

    crud op and image management

--- src/main/java/com/mproduits/model/Product.java
+++ src/main/java/com/mproduits/model/Product.java
@@ -1,21 +1,24 @@
 package com.mproduits.model;
 
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
 public class Product {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)  // Use IDENTITY for H2 auto-increment
     private int id;
     private String titre;
     private String description;
-    private String image;
+    private String image;  // Path to the image file
     private Double prix;
-    public Product() {
-    }
+
+    // Default constructor
+    public Product() {}
+
+    // Parameterized constructor
     public Product(int id, String titre, String description, String image, Double prix) {
         this.id = id;
         this.titre = titre;
@@ -23,17 +26,50 @@ public class Product {
         this.image = image;
         this.prix = prix;
     }
-    public int getId() { return id; }
-    public void setId(int id) { this.id = id; }
-    public String getTitre() { return titre; }
-    public void setTitre(String titre) { this.titre = titre; }
-    public String getDescription() { return description; }
-    public void setDescription(String description) { this.description = description; }
-    public String getImage() { return image; }
-    public void setImage(String image) { this.image = image; }
-    public Double getPrix() { return prix; }
-    public void setPrix(Double prix) { this.prix = prix; }
+
+    // Getters and Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitre() {
+        return titre;
+    }
+
+    public void setTitre(String titre) {
+        this.titre = titre;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public Double getPrix() {
+        return prix;
+    }
+
+    public void setPrix(Double prix) {
+        this.prix = prix;
+    }
+
     @Override
     public String toString() {
-        return "Product{" + "id=" + id + ", titre='" + titre + '\'' + ", description='" + description + '\'' + ",image='" + image + '\'' + ", prix=" + prix + '}'; }
+        return "Product{" + "id=" + id + ", titre='" + titre + '\'' + ", description='" + description + '\'' + ", image='" + image + '\'' + ", prix=" + prix + '}';
+    }
 }
--- src/main/resources/data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO Product (titre, description, image, prix)
-VALUES ('Produit 1', 'Description du produit 1', 'image1.jpg', 199.99),
-       ('Produit 2', 'Description du produit 2', 'image2.jpg', 349.50),
-        ('Produit 3', 'Description du produit 3', 'image3.jpg', 120.75);
