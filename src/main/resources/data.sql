2648027fd880d11a38fa1ee46d5b72114a54b600
manalizo <yumimanal8@gmail.com>
2025-01-07 23:46:18 +0100

    send image

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO Product (titre, description, image, prix)
+VALUES ('car red', 'Description du produit 1', 'images/car2-removebg-preview.png', 199.99),
+       ('car toy', 'Description du produit 2', 'images/car6.png', 349.50),
+        ('car geen', 'Description du produit 3', 'images/car6.png', 120.75);
