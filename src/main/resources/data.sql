be158c1690950f9fff8113ca9bd7f2c804c5d2a8
manalizo <yumimanal8@gmail.com>
2025-01-09 01:11:38 +0100

    add products

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,5 @@
 INSERT INTO Product (titre, description, image, prix)
 VALUES ('car red', 'Description du produit 1', 'images/car2-removebg-preview.png', 199.99),
        ('car toy', 'Description du produit 2', 'images/car6.png', 349.50),
-        ('car geen', 'Description du produit 3', 'images/car6.png', 120.75);
+        ('car geen', 'Description du produit 3', 'images/car.jpeg', 120.75),
+        ('pink car', 'Description du produit 4', 'images/car5.jpeg', 220.75);
